7992ed12186db94c9c5d7c111196979bfadc3218
myushmi <mik.ushakoff2017@yandex.ru>
2025-01-18 15:51:58 +0300

    Update TaskDetailsModal.tsx

--- front/src/Components/TaskDetailsModal.tsx
+++ front/src/Components/TaskDetailsModal.tsx
@@ -66,13 +66,13 @@ export default function TaskDetailsModal({
 			initial={{ opacity: 0 }}
 			animate={{ opacity: 1 }}
 			exit={{ opacity: 0 }}
-			className="fixed inset-0 bg-black bg-opacity-50 w-full flex items-center justify-center z-100 p-4"
+			className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-100 p-4"
 		>
 			<motion.div
 				initial={{ scale: 0.9, opacity: 0 }}
 				animate={{ scale: 1, opacity: 1 }}
 				exit={{ scale: 0.9, opacity: 0 }}
-				className="bg-black border-2 border-gray-500 font-medium rounded-lg max-w-2xl relative flex flex-col"
+				className="bg-black border-2 border-gray-500 font-medium rounded-lg  w-full max-w-2xl relative flex flex-col"
 			>
 				<div className="p-8">
 					<button
