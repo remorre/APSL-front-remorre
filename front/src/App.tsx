f3cc7749924ab42ff1f7d7d74ad585bc3465c941
myushmi <mik.ushakoff2017@yandex.ru>
2025-01-18 16:24:45 +0300

    5

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -44,7 +44,7 @@ function App() {
 				<WalletChecker>
 					<Animate className="fixed inset-0 z-0" />
 					<Header isWalletConnected={isWalletConnected} />
-					<main className="flex-grow">
+					<main className="flex-grow relative z-10">
 						<Routes>
 							<Route path="/" element={<HomePage />} />
 							<Route
